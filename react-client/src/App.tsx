7296009c173a9c0d45b03fa1fbcfdb482f2b720b
last night <fujizhang2525@gmail.com>
2021-01-13 02:43:08 -0500

    deploy to website

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './App.css';
+
 function App() {
-   import('rust-wasm').then(module => {
-      console.log(module)
-   })
+  import('rust-wasm').then(({ add_two_ints, fib }) => {
+      // off-loading computations to WASM
+      const sumResult = add_two_ints(10, 20);
+      const fibResult = fib(10);
+      // updating our sumResult and fibResult values (declared below)
+      setSum(sumResult);
+      setFib(fibResult);
+  });
+   const [sum, setSum] = useState<number>(0);
+   const [fib, setFib] = useState<number>(0);
    return (
       // I cut out the fluff
-      <div className="App" />
+      // Displaying our sum and fib values that're updated by WASM
+      <div className="App" >
+         <div>Sum Results: {sum}</div>
+         <div>Fib Results: {fib}</div>
+      </div>
    );
 }
 export default App;
